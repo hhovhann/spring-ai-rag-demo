43c86fc28d3359d3654d31d6e7961bcc902d0e53
hhovhann <haik.hovhanisyan@gmail.com>
2025-10-02 09:22:47 +0400

    initial commit

---
+++ src/main/java/am/hhovhann/spring_ai_rag_demo/SpringAiRagDemoApplication.java
@@ -0,0 +1,13 @@
+package am.hhovhann.spring_ai_rag_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiRagDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiRagDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/am/hhovhann/spring_ai_rag_demo/SpringAiRagDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package am.hhovhann.spring_ai_rag_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiRagDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
